5c102102c58e9c17cd0393601c4c486115cca58c
BuildTools <unconfigured@null.spigotmc.org>
2020-07-15 12:42:33 +0200

    basic calculator functions and tests

---
+++ Calculator.php
@@ -0,0 +1,21 @@
+<?php
+
+
+class Calculator
+{
+    function add($a, $b) : int {
+        return $a + $b;
+    }
+
+    function substract($a, $b) : int {
+        return $a - $b;
+    }
+
+    function divide($a, $b) : int {
+        return $a / $b;
+    }
+
+    function multiply($a, $b) : int {
+        return $a * $b;
+    }
+}
---
+++ CalculatorTest.php
@@ -0,0 +1,26 @@
+<?php
+use PHPUnit\Framework\TestCase;
+
+
+class CalculatorTest extends TestCase
+{
+    public function testAdd()
+    {
+         $this->assertTrue(true);
+    }
+
+    public function testSubstract()
+    {
+        $this->assertTrue(true);
+    }
+
+    public function testMultiply()
+    {
+        $this->assertTrue(true);
+    }
+
+    public function testDivide()
+    {
+        $this->assertTrue(true);
+    }
+}
